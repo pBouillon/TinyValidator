df6b1191d0da965f4e6a6669d0f33dada9c7c785
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-30 16:05:09 +0200

    Add built ins string validators and default exception

---
+++ TinyValidator/Builtins/String/StringRule.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      TinyValidator - https://github.com/pBouillon/TinyValidator
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/TinyValidator/blob/master/LICENSE
+ */
+
+using TinyValidator.Abstractions;
+
+namespace TinyValidator.Builtins.Strings
+{
+    /// <summary>
+    /// Specialized <see cref="Rule{T}"/> for string validation
+    /// </summary>
+    public abstract class StringRule : Rule<string> { }
+}
