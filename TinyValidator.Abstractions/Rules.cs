800a2010987a730055eaeffc2954c36911bc0cf8
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-30 14:23:43 +0200

    Add TinyValidator abstractions

---
+++ TinyValidator.Abstractions/Rules.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      TinyValidator - https://github.com/pBouillon/TinyValidator
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/TinyValidator/blob/master/LICENSE
+ */
+
+namespace TinyValidator.Abstractions
+{
+    /// <summary>
+    /// Represent a unique rule to be validated by the <see cref="IValidator{T}"/>
+    /// </summary>
+    /// <typeparam name="T">Type of the value to be validated</typeparam>
+    public abstract class Rule<T>
+    {
+        /// <summary>
+        /// Check if the value is valid in its case
+        /// </summary>
+        /// <returns>True if the rule is respected; false otherwise</returns>
+        protected abstract bool IsValid(T value);
+
+        /// <summary>
+        /// Logic to be executed if <see cref="Rule{T}.IsValid"/> fails
+        /// </summary>
+        protected abstract void OnInvalid();
+
+        /// <summary>
+        /// Perform the effective validation
+        /// </summary>
+        /// <param name="value">Value to be validated</param>
+        public void Validate(T value)
+        {
+            if (!IsValid(value))
+            {
+                OnInvalid();
+            }
+        }
+    }
+}
