aff2270622602038631e496c1aa85429ce4f4199
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-30 14:24:14 +0200

    Add TinyValidator initial Validator implementation

---
+++ TinyValidator/Validator.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      TinyValidator - https://github.com/pBouillon/TinyValidator
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/TinyValidator/blob/master/LICENSE
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using TinyValidator.Abstractions;
+
+namespace TinyValidator
+{
+    /// <summary>
+    /// Concrete implementation of <see cref="IValidator{T}"/> 
+    /// </summary>
+    /// <inheritdoc cref="IValidator{T}"/>
+    public class Validator<T> : IValidator<T>
+    {
+        /// <inheritdoc cref="IValidator{T}.Rules"/>
+        public IEnumerable<Rule<T>> Rules { get; }
+
+        /// <summary>
+        /// Create a new validator with its set of rules
+        /// </summary>
+        /// <param name="rules">Rules to be validated</param>
+        private Validator(params Rule<T>[] rules)
+            => Rules = rules;
+
+        /// <summary>
+        /// Create a new validator with a set of rules to be executed in the
+        /// provided order
+        /// </summary>
+        /// <param name="rules">Rules to be validated</param>
+        /// <returns>A new instance of <see cref="Validator{T}"/></returns>
+        public static Validator<T> CreateValidationPipelineWith(params Rule<T>[] rules)
+            => new Validator<T>(rules);
+
+        /// <inheritdoc cref="IValidator{T}.Validate"/>
+        public void Validate(T value)
+            => Rules.ToList()
+                .ForEach(rule =>
+                    rule.Validate(value));
+    }
+}
